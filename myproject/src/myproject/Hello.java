49524bbbdc2ea6fc6d367ee2830f12b0a3f44036
user <user@es302n35>
2018-09-25 11:51:13 +0800

    initial

---
+++ myproject/src/myproject/Hello.java
@@ -0,0 +1,13 @@
+package myproject;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		int age = -1;
+		age = 19;
+		short num = 8;
+		age = num;
+		float weight = (float)52.5;
+		System.out.println(weight);
+	}
+}
